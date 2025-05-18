7f54c983de4085aa52ad5d07593348c08fd62363
albinmaqastena <albin.maqastena@student.uni-pr.edu>
2025-05-18 16:07:02 +0200

    readme and code update

--- Base64.py
+++ Base64.py
@@ -20,7 +20,6 @@ def base64_encode(text):
     encoded += '=' * padding
 
     return encoded
-#
 
 # dekodimi i nje stringu Base64 ne tekst normal, pa perdorur klasa dhe funksione te gatshme
 def base64_decode(encoded_text):
